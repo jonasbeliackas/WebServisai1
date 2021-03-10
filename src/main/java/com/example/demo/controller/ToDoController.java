034806cad58fbf1ff8165077f7be2f81d9bc9501
Jonas <jonas.beliackas06@gmail.com>
2021-03-10 11:31:23 +0200

    [UPD] updated satuses and put get

--- src/main/java/com/example/demo/controller/ToDoController.java
+++ src/main/java/com/example/demo/controller/ToDoController.java
@@ -3,9 +3,11 @@ package com.example.demo.controller;
 import com.example.demo.model.ToDoItem;
 import com.example.demo.repo.ToDoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/todo")
@@ -14,11 +16,39 @@ public class ToDoController {
     private ToDoRepo toDoRepo;
 
     @PostMapping
-    public ToDoItem        save   (@RequestBody ToDoItem toDoItem)  { return toDoRepo.save(toDoItem); }//Create
+    @ResponseStatus(HttpStatus.CREATED)
+    public ToDoItem save(@RequestBody ToDoItem toDoItem) {
+        return toDoRepo.save(toDoItem);
+    }//Create
+
     @GetMapping
-    public List<ToDoItem>  findAll()                                { return toDoRepo.findAll(); }     //READ
+    @ResponseStatus(HttpStatus.FOUND)
+    public List<ToDoItem> findAll() {
+        return toDoRepo.findAll();
+    }//READ
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public Optional<ToDoItem> findByID(@PathVariable Long id) {
+
+        return toDoRepo.findById(id);
+    }//READ
+
     @PutMapping
-    public ToDoItem        update (@RequestBody ToDoItem toDoItem)  { return toDoRepo.save(toDoItem); }//Update
+    public ToDoItem update(@RequestBody ToDoItem toDoItem) {
+        return toDoRepo.save(toDoItem);
+    }//Update
+    @PutMapping(value = "/{id}")
+    public ToDoItem update(@PathVariable Long id,@RequestBody ToDoItem toDoItem) {
+        ToDoItem item = toDoRepo.getOne(id);
+        item.setTitle(toDoItem.getTitle());
+        item.setDone(toDoItem.isDone());
+        return toDoRepo.save(item);
+    }//Update
+
     @DeleteMapping(value = "/{id}")
-    public void            delete (@PathVariable Long id)           { toDoRepo.deleteById(id); }       //Delete
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void delete(@PathVariable Long id) {
+        toDoRepo.deleteById(id);
+    }//Delete
 }
